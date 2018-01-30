f5a630e3a48c90cb7d0b44e30b54aa190e05f419
tsuntheng <35157382+tsuntheng@users.noreply.github.com>
2018-01-29 23:06:21 -0500

    Update RectangleClient.java

--- RectangleClient.java
+++ RectangleClient.java
@@ -2,9 +2,15 @@
 public class RectangleClient {
 	public static void main(String[] args) {
 		
-		Rectangle rect1 = new Rectangle(7,9,10,0);
-		System.out.println("r1: " + rect1);
+		Rectangle rect1 = new Rectangle(2,5,8,10);
+		System.out.println(rect1);
 		
-		
+		if (rect1.contains(5,6)) {
+			System.out.println("True");
+		}
+		else {
+			System.out.println("False");
+		}
 	}
 }
+
