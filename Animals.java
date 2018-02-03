faa61b7fbc6d6e2a4736a6f0d4fc9bd55d9b0d16
tsuntheng <35157382+tsuntheng@users.noreply.github.com>
2018-02-03 00:32:53 -0500

    Add files via upload

---
+++ Animals.java
@@ -0,0 +1,23 @@
+
+public class Animals {
+	public String getFood() {
+		return "They eat anything.";
+	}
+	
+	public void getOffsprings() {
+		System.out.println("Lay eggs");
+	}
+	
+	public void getDiet() {
+		System.out.println("Omnivores");
+	}
+	
+	public int getSleep() {
+		return 8;
+	}
+	
+	public int getSpecies() {
+		return 5000;
+	}
+
+}
